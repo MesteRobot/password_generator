9593c819e547f2cb0dfe16c3cb81f2021d95920d
MesteRobot <hassan.doodiyan@gmail.com>
2021-04-03 22:45:52 +0430

    create first view from templates and route the root page to it

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return render(request, "generator/home.html", {"password": "089fyuasdhkjl"})
