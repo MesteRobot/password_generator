5f826822906adb6a8aea5424fd6dfd0151015500
MesteRobot <hassan.doodiyan@gmail.com>
2021-04-04 21:23:13 +0430

    complete the backend of the password generator

--- generator/views.py
+++ generator/views.py
@@ -8,13 +8,22 @@ def Home(request):
 
 
 def PasswordGenerator(request):
-    lower_chars = list("abcdefghijklmnopqrstuvwxyz")
-    upper_chars = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    specials = list("!@#$%^&*_+<>?;:")
-    numbers = list("0123456789")
+    lower_chars = "abcdefghijklmnopqrstuvwxyz"
+    upper_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    specials = "!@#$%^&*_+<>?;:"
+    numbers = "0123456789"
     generated_password = ""
-    
-    for i in range(10):
-        generated_password += random.choice(lower_chars)
+    password_length = int(request.GET.get('length'))
+
+    pass_seed = lower_chars
+    if request.GET.get('uppercase'):
+        pass_seed += upper_chars
+    if request.GET.get('special'):
+        pass_seed += specials
+    if request.GET.get('number'):
+        pass_seed += numbers
+
+    for i in range(password_length):
+        generated_password += random.choice(list(pass_seed))
 
     return render(request, "generator/password.html", {"password": generated_password})
